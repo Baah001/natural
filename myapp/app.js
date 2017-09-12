4e684d4c8bf2b10c70c6a53dcd4a517e2b823672
s.baah <sethbaah001@gmail.com>
2017-09-12 20:00:39 +0200

    fast commit

--- myapp/app.js
+++ myapp/app.js
@@ -4,7 +4,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-
+const sorry = 5;
 const index = require('./routes/index');
 const users = require('./routes/users');
 
